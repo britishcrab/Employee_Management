2ee3218661cfc3c5dd8e9cf2f9f7dc2a22bc3163
vagrant <vagrant@vagrant>
2018-07-26 10:04:29 +0000

    view変更

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -13,7 +13,7 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        return view('sample');
+        return view('employee.register');
         //
     }
 
