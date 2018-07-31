140dd72b635f3f603ba504898e5da0711f600096
vagrant <vagrant@vagrant>
2018-07-31 03:24:53 +0000

    view編集

--- resources/views/employee/admin.blade.php
+++ resources/views/employee/admin.blade.php
@@ -10,7 +10,7 @@
     <h1>管理<h1>
     <div class="col-xs-8">
         <ul class="nav nav-pills nav-stacked">
-			<input class="btn btn-default btn-block" type="button" onclick="location.href='{{route('admin.list')}}'" value="社員一覧">
+			<input class="btn btn-default btn-block" type="button" onclick="location.href='{{route('admin.get.list')}}'" value="社員一覧">
 			<input class="btn btn-default btn-block" type="button" onclick="location.href='laravel'" value="日報一覧">
         </ul>
     </div>
