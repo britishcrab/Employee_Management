32d78a023990c0b15a371012c14709724cdf9fa5
vagrant <vagrant@vagrant.vm>
2018-08-24 11:23:59 +0900

    test signin

--- tests/Feature/SignInTest.php
+++ tests/Feature/SignInTest.php
@@ -13,7 +13,6 @@ use App\Models\Employee;
 
 class SignInTest extends TestCase
 {
-
     /**
      * @test
      * サインイン画面の表示
@@ -26,67 +25,20 @@ class SignInTest extends TestCase
 
     /**
      * @test
-     */
-    public function unvalid_password_can_login()
-    {
-        /**
-         * サインイン失敗
-         * メール     正
-         * パスワード 不正
-         */
-        // ユーザーを１つ作成
-        $user = factory(Employee::class)->create(['password'  => Hash::make('password')]);
-        // パスワード間違いでログインを実行
-        $response = $this->post('auth/signin', [
-            'mail' => $user->mail,
-            'password' => 'miss'
-        ]);
-        // 認証されていない
-        $this->assertFalse(Auth::guard()->check(), 'password間違いですが認証されています');
-        // セッションにサインイン失敗のエラーが入っているか
-        $response->assertSessionHas('errors');
-        $response->assertSessionHasErrors(['signin_error' => 'サインインに失敗しました.']);
-
-        /**
-         * サインイン失敗
-         * メール     不正
-         * パスワード 正
-         */
-        // パスワード間違いでログインを実行
-        $response = $this->post('auth/signin', [
-            'mail' => 'miss@gmail.com',
-            'password' => 'password'
-        ]);
-        // 認証されていない
-        $this->assertFalse(Auth::guard()->check(), 'password間違いですが認証されています');
-        // セッションにサインイン失敗のエラーが入っているか
-        $response->assertSessionHas('errors');
-        $response->assertSessionHasErrors(['signin_error' => 'サインインに失敗しました.']);
-    }
-
-    /**
-     * @test
      * サインイン失敗
      * メール     不正
      * パスワード 正
      */
     public function unvalid_mail_can_login()
     {
-        // ユーザーを１つ作成
         $user = factory(Employee::class)->create([
             'password'  => Hash::make('password')
         ]);
-
-        // パスワード間違いでログインを実行
         $response = $this->post('auth/signin', [
             'mail' => 'miss@gmail.com',
             'password' => 'password'
         ]);
-
-        // 認証されていない
         $this->assertFalse(Auth::guard()->check(), 'password間違いですが認証されています');
-
-        // セッションにサインイン失敗のエラーが入っているか
         $response->assertSessionHas('errors');
         $response->assertSessionHasErrors(['signin_error' => 'サインインに失敗しました.']);
     }
@@ -99,21 +51,14 @@ class SignInTest extends TestCase
      */
     public function unvalid_mail_password_can_login()
     {
-        // ユーザーを１つ作成
         $user = factory(Employee::class)->create([
             'password'  => Hash::make('password')
         ]);
-
-        // パスワード間違いでログインを実行
         $response = $this->post('auth/signin', [
             'mail' => 'miss@gmail.com',
             'password' => 'miss'
         ]);
-
-        // 認証されていない
         $this->assertFalse(Auth::guard()->check(), 'password間違いですが認証されています');
-
-        // セッションにサインイン失敗のエラーが入っているか
         $response->assertSessionHas('errors');
         $response->assertSessionHasErrors(['signin_error' => 'サインインに失敗しました.']);
     }
@@ -124,24 +69,16 @@ class SignInTest extends TestCase
      */
     public function valid_user_can_login()
     {
-        // ユーザーを１つ作成
         $user = factory(Employee::class)->create([
             'password'  => Hash::make('password')
         ]);
-
-        // まだ、認証されていない
         $this->assertFalse(Auth::guard()->check(), '認証処理前に認証されています');
 
-        // ログインを実行
         $response = $this->post('auth/signin', [
             'mail'    => $user->mail,
             'password' => 'password'
         ]);
-
-        // 認証されている
         $this->assertTrue(Auth::guard()->check(), '認証処理後に認証されていません');
-
-        // ログイン後にホームページにリダイレクトされるのを確認
         $response->assertRedirect('/');
     }
 }
